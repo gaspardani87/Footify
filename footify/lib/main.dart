3d59d0a7f8531993fab23e35178bc46c338f5898
Daniel <gaspardani87@gmail.com>
2025-02-02 01:26:10 +0100

    Alap assetek feltöltése és inicializálása

--- footify/lib/main.dart
+++ footify/lib/main.dart
@@ -11,9 +11,28 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        backgroundColor: Color(0xFF1D1D1D),
+        body: Header(),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(0),
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/Footify-Logo-NoBG-LightMode.png',
+            width: 150,
+            height: 150,
+          ),
+        ],
       ),
     );
   }
