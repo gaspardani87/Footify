199e8a806726a0dd41623eca85ce012c671b36d6
Daniel <gaspardani87@gmail.com>
2025-02-21 00:28:44 +0100

    Firebase dependecy-k importálása, alap kód megírása a világos témához

---
+++ footify/lib/theme_notifier.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  bool _isDarkMode = true;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeData get currentTheme => _isDarkMode ? _darkTheme : _lightTheme;
+
+  void toggleTheme(bool isDark) {
+    _isDarkMode = isDark;
+    notifyListeners();
+  }
+
+  static final ThemeData _lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: const Color(0xFFFFE6AC),
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFFFFE6AC),
+      iconTheme: IconThemeData(color: Colors.black),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.black),
+      bodyMedium: TextStyle(color: Colors.black),
+    ),
+    iconTheme: const IconThemeData(color: Colors.black),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Color(0xFFFFE6AC),
+      unselectedItemColor: Colors.black,
+    ),
+  );
+
+  static final ThemeData _darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: const Color(0xFFFFE6AC),
+    scaffoldBackgroundColor: const Color(0xFF1D1D1D),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF1D1D1D),
+      iconTheme: IconThemeData(color: Colors.white),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+      bodyMedium: TextStyle(color: Colors.white),
+    ),
+    iconTheme: const IconThemeData(color: Colors.white),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Color(0xFFFFE6AC),
+      unselectedItemColor: Colors.white,
+    ),
+  );
+}
